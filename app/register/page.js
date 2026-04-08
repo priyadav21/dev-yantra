6536f156cc5a421db3bd49e05a64822fc9e27129
PriyaYadav <priyadav218@gmail.com>
2026-04-08 22:14:31 +0530

    basic structure of form

--- app/register/page.js
+++ app/register/page.js
@@ -1,10 +1,17 @@
 import Nav from "../components/Nav";
 
-export default function Register  () {
-    return (
-        <Nav>
-        <h1>Register</h1>
-        <p>Register form will go here.</p> 
-        </Nav>
-    );
+export default function Register() {
+  const registerForm = () => {
+    // Logic for handling form submission will go here
+    return <form>Form</form>;
+  };
+
+  return (
+    <Nav className="w-full justify-center items-center">
+      <div className="w-full max-w-md mx-auto mt-8">
+        <h1 className="text-6xl"> Register</h1>
+        <div className="w-full max-w-md mx-auto mt-8">{registerForm()}</div>
+      </div>
+    </Nav>
+  );
 }
